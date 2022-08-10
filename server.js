acec516917559995dfe67fac15a5c028cf6519df
Ivana <ivana.a.mayberry@gmail.com>
2022-08-10 12:15:55 -0500

    #commit

--- server.js
+++ server.js
@@ -10,11 +10,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', route);
 
-mongoose.connect('mongodb+srv://imayb:iWB9IRVm4Rkh5r3Y@cluster0.p7jcgwt.mongodb.net/portfolioDB', {useNewURLParser: true, useUnifiedTopology: true})
-.then(() => {
-    app.listen(3000, () => {
-        console.log("server is running on 3000")
-    })
-})
+// mongoose.connect('mongodb+srv://imayb:iWB9IRVm4Rkh5r3Y@cluster0.p7jcgwt.mongodb.net/portfolioDB', {useNewURLParser: true, useUnifiedTopology: true})
+// .then(() => {
+//     app.listen(3000, () => {
+//         console.log("server is running on 3000")
+//     })
+// })
+
+mongoose.connect(process.env.MONGODB_URI, {useNewURLParser: true, useUnifiedTopology: true}) 
+.then(() => { 
+    app.listen(process.env.PORT || 3000, () => { 
+        console.log("Server has started running on 3000") 
+    }) 
+}) 
 
 // iWB9IRVm4Rkh5r3Y
